d49818db5700851de0308c26470eec509c6bb814
Mohd Jalaluddin Ahmad <9w2tja@gmail.com>
2019-04-01 19:57:16 +0800

    Create tts.py

---
+++ tts.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+message = 'Hello World! My name is Shahrul Nizam from Malaysia.'
+
+print('Festival Text to Speech')
+os.system('echo "' + 'Festival Text to Speech demo' + '" | festival --tts')
+time.sleep(1)
+os.system('echo "' + message + '" | festival --tts')
+time.sleep(1)
+
+print('Espeak Text to Speech')
+os.system('espeak -ven+f3 -k5 -s150 "' + 'Espeak Text to Speech demo' + '"')
+time.sleep(1)
+os.system('espeak -ven+f3 -k5 -s150 "' + message + '"')
+time.sleep(1)
+
+print('Pico Text to Speech')
+os.system('pico2wave -w voice.wav "' + 'Pico Text to Speech demo' + '" && aplay voice.wav')
+time.sleep(1)
+os.system('pico2wave -w voice.wav "' + message + '" && aplay voice.wav')
+time.sleep(1)
